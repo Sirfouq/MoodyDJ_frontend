bc72a3423baa2bbfdf6489a7f37cceb3395d0e01
giorgoskokkinos <kokkinos1@gmail.com>
2025-10-02 14:09:33 +0200

    Modify file structure for reusability LoginButton modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
 
-
-
 import './App.css'
+import { LoginCard } from './LoginCard';
 
 function App() {
 
@@ -14,34 +13,5 @@ function App() {
 
 }
 
-
-const LoginCard = () => {
-
-  return (
-    // This is the main white card with shadow and padding
-    <div className="bg-white p-8 sm:p-12 rounded-2xl shadow-xl w-full max-w-md text-center">
-      <h1 className="text-2xl sm:text-3xl font-bold text-gray-800 mb-6">
-        Welcome to MoodyDJ
-      </h1>
-      <p className="text-gray-500 mb-8">
-        Log in to start creating playlists from your mood.
-      </p>
-      <LoginButton />
-    </div>
-  );
-}
-
-
-const LoginButton = () => {
-  return (
-    <button
-
-      onClick={() => alert('Button clicked!')}
-      className="inline-block bg-green-500 text-white font-bold text-lg px-8 py-4 rounded-lg shadow-md hover:bg-green-600 transition-colors duration-300"
-    >
-      Login with Spotify
-    </button>
-  );
-};
 
 export default App
---
+++ src/LoginCard.tsx
@@ -0,0 +1,31 @@
+import spotifyIcon from './assets/spotify-color-svgrepo-com.svg';
+
+
+export const LoginCard = () => {
+
+    return (
+
+        <div className="bg-white p-8 sm:p-12 rounded-2xl shadow-xl w-full max-w-md text-center">
+            <h1 className="text-2xl sm:text-3xl font-bold text-gray-800 mb-6">
+                Welcome to MoodyDJ
+            </h1>
+            <p className="text-gray-500 mb-8">
+                Log in to start creating playlists from your mood.
+            </p>
+            <LoginButton />
+        </div>
+    );
+};
+const LoginButton = () => {
+    return (
+        <button
+
+            onClick={() => alert('Button clicked!')}
+            className="inline-flex items-center justify-center gap-3 bg-green-600 text-white font-bold text-lg px-8 py-4 rounded-lg shadow-md hover:bg-green-800 transition-colors duration-300">
+            <img src={spotifyIcon} alt="Spotify" className="w-6 h-6" />
+
+            Login with Spotify
+
+        </button>
+    );
+};
