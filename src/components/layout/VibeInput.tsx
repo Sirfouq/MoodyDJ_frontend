fbc115d710c9a9544b12083ed64511595db5d555
giorgoskokkinos <kokkinos1@gmail.com>
2026-05-12 21:44:04 +0200

    -Extract Input logic in separate component -Add new slider from shadcn library

--- src/components/layout/VibeInput.tsx
+++ src/components/layout/VibeInput.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { Textarea } from '@/components/ui/textarea'
+import { useState } from 'react'
+import { ArrowRight } from 'lucide-react';
 
-const VibeInput = () => {
+interface inputProps {
+  onSubmit: (input: string) => void
+}
+
+const VibeInput = ({ onSubmit }: inputProps) => {
+  const [input, setInput] = useState('');
+  const isVisible = input.trim() !== '';
+
+
+  const handleSubmit = () => {
+    onSubmit(input)
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      handleSubmit();
+    }
+  }
+
   return (
-    <div>VibeInput</div>
+    <div className='relative w-full max-w-3xl'>
+      <Textarea placeholder='How are you feeling today ?'
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+        onKeyDown={handleKeyDown} />
+      <button className={`absolute bottom-4 right-3 p-1.5 rounded-lg             
+  bg-indigo-500 text-white hover:bg-indigo-300 active:scale-90 transition-all duration-200 ${isVisible ? 'opacity-100 scale-100' : 'opacity-0 scale-75 pointer-events-none'}`}
+        onClick={handleSubmit}>
+        <ArrowRight size={20} />
+      </button>
+    </div>
   )
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,12 @@
-import { Textarea } from '@/components/ui/textarea'
 import { useContext, useState } from 'react'
-import { ArrowRight } from 'lucide-react';
 import { generate_playlist, type Track } from '@/services/backend_api_comm';
 import useSpotifyPlayer from '@/hooks/useSpotifyPlayer';
 import { AuthContext } from '@/contexts/AuthContext';
+import VibeInput from '@/components/layout/VibeInput';
 
 
 export const HomePage = () => {
 
-  const [input, setInput] = useState('');
-  const isVisible = input.trim() !== '';
   const [tracks, setTracks] = useState<Track[]>([])
   const { access_token } = useContext(AuthContext)
   const { player, deviceId } = useSpotifyPlayer(access_token ?? '')
@@ -17,32 +14,15 @@ export const HomePage = () => {
 
 
 
-  const handleSubmit = async () => {
-    setTracks([])
-    const tracks = await generate_playlist(input)
-    setTracks(tracks)
-  }
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.key === 'Enter' && !e.shiftKey) {
-      e.preventDefault();
-      handleSubmit();
-    }
-  }
 
   return (
     <div className='flex flex-col items-center justify-center  w-full px-4'>
-      <div className='relative w-full max-w-3xl'>
-        <Textarea placeholder='How are you feeling today ?'
-          onChange={(e) => setInput(e.target.value)}
-          value={input}
-          onKeyDown={handleKeyDown} />
-        <button className={`absolute bottom-4 right-3 p-1.5 rounded-lg             
-  bg-indigo-500 text-white hover:bg-indigo-300 active:scale-90 transition-all duration-200 ${isVisible ? 'opacity-100 scale-100' : 'opacity-0 scale-75 pointer-events-none'}`}
-          onClick={handleSubmit}>
-          <ArrowRight size={20} />
-        </button>
-      </div>
+      <VibeInput onSubmit={async (input) => {
+        setTracks([])
+        const tracks = await generate_playlist(input)
+        setTracks(tracks)
+      }}></VibeInput>
 
 
       {`Device Id : ${deviceId}`}
